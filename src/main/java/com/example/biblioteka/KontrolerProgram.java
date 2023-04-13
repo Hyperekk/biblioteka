18753873912a07873ad4e68d921c65a684fa7670
adik08pl <adik08.email@gmail.com>
2023-04-13 23:10:12 +0200

    commit 4 ksaizki z pliku sie wczytuja, aczykolwiek sie nie przeladowuja

--- src/main/java/com/example/biblioteka/KontrolerProgram.java
+++ src/main/java/com/example/biblioteka/KontrolerProgram.java
@@ -1,6 +1,8 @@
 package com.example.biblioteka;
 
+import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 import java.io.IOException;
@@ -13,31 +15,67 @@ import java.util.List;
 public class KontrolerProgram
 {
     public Button btnLewo;
+
+    public TextField txtUser;
     public Button btnPrawo;
     public TextField txt1, txt2, txt3, txt4, txt5, txt6;
 
+    public List<Rozdzielacz> ksiazki = new ArrayList<>();
+    public Button btnDodaj;
+    public Button btnZamień;
+    public Button btnUsuń;
+    public Button btnZapisz;
+    public Button btnAnuluj;
+    public Label lb1;
+
+    public int numerKsiazki = 0;
+
     public void initialize() throws IOException {
-        Path path = Paths.get("C://biblioteka.txt");
+        //String login = txtUser.getText();
+        //lb1.setText(login);
+
+        Path path = Paths.get("C:/Users/Bob/Desktop/Biblioteka.txt");
         if (!Files.exists(path))
             Files.createFile(path);
 
         List<String> linie = Files.readAllLines(path);
-        List<Object> ksiazki = new ArrayList<>();
 
         for (String linia : linie )
         {
             String[] parts = linia.split("#");
-            Rozdzielacz rozdzielacz = new Rozdzielacz(parts[1], parts[2], parts[3], parts[4], parts[5], parts[6]);
+            Rozdzielacz rozdzielacz = new Rozdzielacz(parts[0], parts[1], parts[2], parts[3], parts[4], parts[5]);
             ksiazki.add(rozdzielacz);
-        }
-    /*
-        String tesktDoZapisania = "";
-        for (Object rozdzielacz : ksiazki)
-        {
-            tesktDoZapisania += rozdzielacz.tytuł
-        }
-    */
 
+            txt1.setText(ksiazki.get(numerKsiazki).tutul = parts[0]);
+            txt2.setText(ksiazki.get(numerKsiazki).IBSBN = parts[1]);
+            txt3.setText(ksiazki.get(numerKsiazki).autor = parts[2]);
+            txt4.setText(ksiazki.get(numerKsiazki).rokWydania = parts[3]);
+            txt5.setText(ksiazki.get(numerKsiazki).wydawnictwo = parts[4]);
+            txt6.setText(ksiazki.get(numerKsiazki).opis = parts[5]);
+
+        }
     }
 
+    public void btnPrawoLewo(ActionEvent actionEvent)
+    {
+        ksiazki.get(numerKsiazki - 1);
+
+        txt1.setText(ksiazki.get(numerKsiazki).tutul);
+        txt2.setText(ksiazki.get(numerKsiazki).IBSBN);
+        txt3.setText(ksiazki.get(numerKsiazki).autor);
+        txt4.setText(ksiazki.get(numerKsiazki).rokWydania);
+        txt5.setText(ksiazki.get(numerKsiazki).wydawnictwo);
+        txt6.setText(ksiazki.get(numerKsiazki).opis);
+    }
+    public void btnPrawoRuch(ActionEvent actionEvent)
+    {
+        ksiazki.get(numerKsiazki + 1);
+
+        txt1.setText(ksiazki.get(numerKsiazki).tutul);
+        txt2.setText(ksiazki.get(numerKsiazki).IBSBN);
+        txt3.setText(ksiazki.get(numerKsiazki).autor);
+        txt4.setText(ksiazki.get(numerKsiazki).rokWydania);
+        txt5.setText(ksiazki.get(numerKsiazki).wydawnictwo);
+        txt6.setText(ksiazki.get(numerKsiazki).opis);
+    }
 }
--- src/main/java/com/example/biblioteka/Rozdzielacz.java
+++ src/main/java/com/example/biblioteka/Rozdzielacz.java
@@ -2,16 +2,17 @@ package com.example.biblioteka;
 
 public class Rozdzielacz
 {
-    public String tutuł;
+    public String tutul;
     public String IBSBN;
     public String autor;
     public String rokWydania;
     public String wydawnictwo;
     public String opis;
 
-    public Rozdzielacz(String tutuł,String IBSBN, String autor, String rokWydania, String wydawnictwo, String opis)
+    public Rozdzielacz(String tutul,String IBSBN, String autor, String rokWydania, String wydawnictwo, String opis)
     {
-        this.tutuł = tutuł;
+        super();
+        this.tutul = tutul;
         this.IBSBN = IBSBN;
         this.autor = autor;
         this.rokWydania = rokWydania;
